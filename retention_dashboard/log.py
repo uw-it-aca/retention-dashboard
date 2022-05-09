b4466b31e65b0d3cfb29c4740c66fe600740765f
Stephen De Vight <devights@uw.edu>
2022-05-09 15:59:32 -0700

    fix

--- retention_dashboard/log.py
+++ retention_dashboard/log.py
@@ -8,7 +8,7 @@ class UserFilter(Filter):
     """ Add user information to each log entry. """
 
     def filter(self, record):
-        from uqgity qserservice.user import UserService
+        from userservice.user import UserService
         user_service = UserService()
         try:
             record.user = user_service.get_original_user() or "-"
